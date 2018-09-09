95cd23118eb8477cd0bb35396e8b7551dae438a9
Dawit Abera <dawitabera64@gmail.com>
2018-09-09 10:15:59 -0700

    admin page added

--- lib/pages/product_admin.dart
+++ lib/pages/product_admin.dart
@@ -1,35 +1,51 @@
 import 'package:flutter/material.dart';
 
 import './home.dart';
+import './product_create.dart';
+import './product_list.dart';
 
 class ProductAdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-          child: Column(children: <Widget>[
-        AppBar(
-          automaticallyImplyLeading: false,
-          title: Text('Choose'),
-        ),
-        ListTile(
-          title: Text('All Products'),
-          onTap: () {
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                  builder: (
-                BuildContext context,
-              ) =>
-                      HomePage()),
-            );
-          },
-        )
-      ])),
-      appBar: AppBar(
-        title: Text('Manage Products'),
-      ),
-      body: Center(child: Text('Mange your products')),
-    );
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          drawer: Drawer(
+              child: Column(children: <Widget>[
+            AppBar(
+              automaticallyImplyLeading: false,
+              title: Text('Choose'),
+            ),
+            ListTile(
+              title: Text('All Products'),
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                      builder: (
+                    BuildContext context,
+                  ) =>
+                          HomePage()),
+                );
+              },
+            )
+          ])),
+          appBar: AppBar(
+            title: Text('Manage Products'),
+            bottom: TabBar(tabs: <Widget>[
+              Tab(
+                icon: Icon(Icons.create),
+                text: 'Create Product',
+              ),
+              Tab(
+                icon: Icon(Icons.list),
+                text: 'My Product',
+              )
+            ]),
+          ),
+          body: TabBarView(
+            children: <Widget>[ProductCreatePage(), ProductListPage()],
+          ),
+        ));
   }
 }
