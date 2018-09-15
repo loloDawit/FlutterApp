fa9893acbce843c149bc5337e3c34955ac92deaa
Dawit Abera <dawitabera64@gmail.com>
2018-09-15 13:54:56 -0700

    scope class added

---
+++ lib/scoped_models/products.dart
@@ -0,0 +1,58 @@
+import 'package:scoped_model/scoped_model.dart';
+import '../models/product.dart';
+
+class ProductsModel extends Model {
+  List<Product> _products = [];
+  int _selectedProductIndex;
+
+  List<Product> get products {
+    return List.from(_products);
+  }
+
+  int get selectedProductIndex {
+    return _selectedProductIndex;
+  }
+
+  Product get selectedProduct {
+    if (_selectedProductIndex == null) {
+      return null;
+    }
+    return _products[_selectedProductIndex];
+  }
+
+  void addProduct(Product product) {
+    _products.add(product);
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void removeProduct() {
+    _products.removeAt(_selectedProductIndex);
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void updateProduct(Product product) {
+    _products[_selectedProductIndex] = product;
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+
+  void selectProduct(int index) {
+    _selectedProductIndex = index;
+  }
+
+  void favProduct() {
+    final bool isCurFav = selectedProduct.isFav;
+    final newFavStatus = !isCurFav;
+    final Product updatedProduct = Product(
+        title: selectedProduct.title,
+        description: selectedProduct.description,
+        price: selectedProduct.price,
+        image: selectedProduct.image,
+        isFav: newFavStatus);
+    _products[_selectedProductIndex] = updatedProduct;
+    _selectedProductIndex = null;
+    notifyListeners();
+  }
+}
